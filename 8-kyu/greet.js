2961d268dea063b0e273ebeaa94e8f2660f5ded0
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-05-26 17:19:09 +0300

    complete greet

---
+++ 8-kyu/greet.js
@@ -0,0 +1,28 @@
+function greet(language) {
+
+    let obj = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+
+    if (obj.hasOwnProperty(language)) {
+        return obj[language];
+    } else {
+        return obj.english;
+    }
+}
